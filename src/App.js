d94bc53fcbf62e8ffa74cf3d22413704bd664edd
JP Rodrigues <70jprodrigues@gmail.com>
2021-04-22 11:54:23 -0300

    Change to hashRoute

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
-import {
-  BrowserRouter as Router,
-  Switch,
-} from "react-router-dom";
+import { HashRouter as Router, Switch } from "react-router-dom";
 
-import routes from "./routerex.js"
+import routes from "./routerex.js";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Switch>
-          {routes()}
-        </Switch>
+        <Switch>{routes()}</Switch>
       </div>
     </Router>
   );
